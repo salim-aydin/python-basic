eca4bf048c7e2491a720059779a633ce802193d5
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-07-19 11:28:04 +0300

    Create dataScience1.py

---
+++ dataScience1.py
@@ -0,0 +1,19 @@
+
+# Çift sayıların karesi alınarak bir sözlüğe eklenmesi istenmektedir.
+# Key' ler orjinal değerler , value'ler ise değiştirilmiş değerler
+
+numbers = range(10)
+new_dict = {}
+for n in numbers:
+    if n % 2 == 0:
+        new_dict[n] = n ** 2
+print(new_dict)
+
+kanks = {n : n ** 2 for n in numbers if n % 2 == 0}
+print(kanks)
+
+
+
+
+
+
